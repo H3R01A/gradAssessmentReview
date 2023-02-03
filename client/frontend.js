eea2a9ff093d970d7986e2c3304b039c1d0b0b7b
H3R01A <aallen0001@gmail.com>
2023-02-02 22:04:12 -0500

    items render to page on load. still need fix when adding a task original state doesn't retain

--- client/frontend.js
+++ client/frontend.js
@@ -15,12 +15,23 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((data) => {
       console.log("loaded data", data);
 
+      //const taskList = document.querySelector("#itemList");
+
+      const itemsContainer = document.getElementById("itemList");
+      let itemHTML = "";
+
       //TODO: Try to add data here...
+      data.forEach((task) => {
+     
+
+        add(task.name, task.status);
+
+      });
     })
     .catch((err) => {
       console.log("this is err", err);
       alert("being redirected to login page because of an error");
-      window.location.replace("http://localhost:3000/");
+      // window.location.replace("http://localhost:3000/");
     });
 });
 
@@ -44,6 +55,7 @@ function todo(container) {
 
   //create an itemList
   const itemList = document.createElement("ul");
+  itemList.setAttribute("id", "itemList");
 
   //!READ FUNCTIONALITY
   //TODO: create fetch funcionality to read values from database
@@ -100,58 +112,91 @@ function todo(container) {
     form.reset();
   });
 
-  /**
-   *
-   * !ADD FUNCTION - to actually put a new task to the screen
-   */
-  function add(input, status) {
-    console.log(input);
-    //create an li element to be a "todo" item
-    const item = document.createElement("li");
 
-    //create an input element to be used when we need to update a task
-    const itemInput = document.createElement("input");
+}
 
-    //set the value of the input field to be the value of the item added
-    itemInput.setAttribute("value", input);
+/**
+ *
+ * !ADD FUNCTION - to actually put a new task to the screen
+ */
+function add(input, status) {
 
-    //create checkbox to append to item as well
-    const itemCheckbox = document.createElement("input");
-    itemCheckbox.setAttribute("type", "checkbox");
-    itemCheckbox.checked = status ? true : false;
 
-    //add class to checkbox
-    itemCheckbox.classList.add("checkbox-todo");
+  console.log(input);
+  //create an li element to be a "todo" item
+  const item = document.createElement("li");
 
-    const deleteButton = document.createElement("button");
-    deleteButton.setAttribute("id", "btn-delete");
-    deleteButton.innerText = "Delete";
+  //create an input element to be used when we need to update a task
+  const itemInput = document.createElement("input");
 
-    //append to the item the input field and the checkbox
+  //set the value of the input field to be the value of the item added
+  itemInput.setAttribute("value", input);
 
-    item.appendChild(itemInput);
-    item.appendChild(itemCheckbox);
-    item.appendChild(deleteButton);
-    item.setAttribute("id", "todoItem");
+  //create checkbox to append to item as well
+  const itemCheckbox = document.createElement("input");
+  itemCheckbox.setAttribute("type", "checkbox");
+  itemCheckbox.checked = status ? true : false;
 
-    //text-decoration: line-through;
+  //add class to checkbox
+  itemCheckbox.classList.add("checkbox-todo");
 
-    //! UPDATE FUNCTIONALITY - checkbox to say status is done
-    itemCheckbox.addEventListener("change", (e) => {
-      console.log("wohoo checkbox clicked!");
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id", "btn-delete");
+  deleteButton.innerText = "Delete";
 
-      console.log(`this is the value of the checkbox ${e.target.checked}`);
-      //TODO: update below code to send value of checkmark to backend
+  //append to the item the input field and the checkbox
 
-      //TODO: fix this part
+  item.appendChild(itemInput);
+  item.appendChild(itemCheckbox);
+  item.appendChild(deleteButton);
+  item.setAttribute("id", "todoItem");
+
+  //text-decoration: line-through;
+
+  //! UPDATE FUNCTIONALITY - checkbox to say status is done
+  itemCheckbox.addEventListener("change", (e) => {
+    console.log("wohoo checkbox clicked!");
+
+    console.log(`this is the value of the checkbox ${e.target.checked}`);
+    //TODO: update below code to send value of checkmark to backend
+
+    //TODO: fix this part
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        taskName: itemInput.value,
+        taskStatus: e.target.checked,
+      }),
+    };
+
+    fetch("http://localhost:3000/api/update", options)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        //append the most recent item to bottom of the form items
+      });
+  });
+
+  //! UPDATE FUNCTIONALITY - change the name of the item
+  itemInput.addEventListener("keypress", (e) => {
+    //preventing from submitting the form and creating a new task
+    e.preventDefault();
+
+    //condition to look for enter key
+    if ((e.key = "enter")) {
+      console.log("you edited the input value");
+      //TODO: update functionality to send updated value to backend
+
       const options = {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          taskName: itemInput.value,
-          taskStatus: e.target.checked,
+          nameUpdate: itemInput.value,
         }),
       };
 
@@ -161,71 +206,43 @@ function todo(container) {
           console.log(data);
           //append the most recent item to bottom of the form items
         });
-    });
+    }
+  });
 
-    //! UPDATE FUNCTIONALITY - change the name of the item
-    itemInput.addEventListener("keypress", (e) => {
-      //preventing from submitting the form and creating a new task
-      e.preventDefault();
+  deleteButton.addEventListener("click", (e) => {
+    e.preventDefault();
 
-      //condition to look for enter key
-      if ((e.key = "enter")) {
-        console.log("you edited the input value");
-        //TODO: update functionality to send updated value to backend
+    //TODO: fix this part
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        taskName: itemInput.value,
+      }),
+    };
 
-        const options = {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            nameUpdate: itemInput.value,
-          }),
-        };
+    fetch("http://localhost:3000/api/delete", options)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        //append the most recent item to bottom of the form items
+      });
 
-        fetch("http://localhost:3000/api/update", options)
-          .then((data) => data.json())
-          .then((data) => {
-            console.log(data);
-            //append the most recent item to bottom of the form items
-          });
-      }
-    });
+    item.remove();
+  });
 
-    deleteButton.addEventListener("click", (e) => {
-      e.preventDefault();
+  //append the item to underneath the <ul> element
+  //https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
 
-      //TODO: fix this part
-      const options = {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          taskName: itemInput.value,
-        }),
-      };
+  const itemsContainer = document.getElementById("itemList");
+  itemsContainer.insertAdjacentElement("afterbegin", item);
 
-      fetch("http://localhost:3000/api/delete", options)
-        .then((data) => data.json())
-        .then((data) => {
-          console.log(data);
-          //append the most recent item to bottom of the form items
-        });
-
-      item.remove();
-    });
-
-    //append the item to underneath the <ul> element
-    //https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
-
-    itemList.insertAdjacentElement("afterbegin", item);
-
-    /**
+  /**
      * ALTERNATIVE METHOD TO APPEND CHILD ELEMENT
      * 
     // itemList.appendChild(item);
 
      */
-  }
 }
